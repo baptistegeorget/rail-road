40eea78c9e8f805a93015e71beae36c480a45e0e
Baptiste GEORGET <baptiste.georget@hotmail.com>
2023-12-03 13:30:05 +0100

    Ajout du middleware express.json()

--- index.ts
+++ index.ts
@@ -1,12 +1,19 @@
+// Imports
 import express from "express"
 
+// Déclarations
 const app = express()
 const port = 3000
 
+// Middlewares
+app.use(express.json())
+
+// Routes
 app.get("/", (req, res) => {
     res.send("Hello world !")
 })
 
+// Lancement du serveur
 app.listen(port, () => {
     console.log(`[SERVEUR] Serveur lancé sur le port ${port}`)
 })
